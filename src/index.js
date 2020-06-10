d6476b3b8d913de130e4545a5047818c1d045683
Javier Leon Cotonieto <>
2020-06-10 07:32:39 -0500

    Hola mundo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//Import modules and Libs
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Create component
+/*const App = () => {
+    return <div>Hola mundo</div>
+}*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Hola</h1><h2>mundo</h2>
+            </div>
+        )
+    }
+}
+
+//Render
+ReactDom.render(
+    <App/>,
+    document.querySelector('#root')
+)
